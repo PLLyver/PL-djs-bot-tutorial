85132f39b3ae48338331cfa38bf94bcf32da3cc4
PLLyver <129743276+PLLyver@users.noreply.github.com>
2023-04-05 23:05:27 +0800

    Delete index.js

--- commands/hi/index.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from ('discord.js');
-
-export const command = new SlashCommandBuilder()
- .setName('sayhi')
- .setDescription('跟我說Hi')
-
-export const action = async (ctx) => {
-   ctx.reply('嗨!')
- }
